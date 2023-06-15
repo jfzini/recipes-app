1366d01d8f56496e328478405a1c58d01d704b98
Jeoflan <jjjeoflan@gmail.com>
2023-06-15 19:01:35 -0300

    continuando os testes done recipes

--- src/tests/RecipeDone.test.js
+++ src/tests/RecipeDone.test.js
@@ -22,90 +22,81 @@ describe('Test if RecipeDone page is working correctly', () => {
 
   it('ready meal should work correctly', async () => {
     const { history } = renderWithRouterAndContext(<App />, '/meals');
-
     await waitFor(() => {
       const firstRecipeButton = screen.getByTestId(firstRecipeBtnID);
       userEvent.click(firstRecipeButton);
     });
-
     expect(history.location.pathname).toBe(mealPathname);
-
     await waitFor(() => {
       const startBtn = screen.getByRole('button', { name: /start recipe/i });
       userEvent.click(startBtn);
     });
-
     expect(history.location.pathname).toBe('/meals/52977/in-progress');
+    await waitFor(() => {
+      const checkBoxes = screen.getAllByRole('checkbox');
+      expect(checkBoxes.length).toBe(13);
+      checkBoxes.forEach((checkBox) => {
+        userEvent.click(checkBox);
+      });
+    });
+    const finishBtn = screen.getByTestId(/finish-recipe-btn/);
+    userEvent.click(finishBtn);
+    expect(history.location.pathname).toBe('/done-recipes');
 
     await waitFor(() => {
-      expect(screen.getByRole('heading', { name: /corba/i })).toBeInTheDocument();
-      expect(screen.getByRole('img', { name: /corba/i })).toHaveProperty('src', 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg');
-      expect(screen.getByRole('button', { name: /share icon/i })).toBeInTheDocument();
-      expect(screen.getByRole('button', { name: /favorite icon/i })).toBeInTheDocument();
-      expect(screen.getByRole('img', { name: /favorite icon/i })).toHaveProperty('src', whiteHeartIcon);
-      expect(screen.getByTestId('recipe-category')).toHaveTextContent('Side');
-      expect(screen.getByText(/lentils - 1 cup/i)).toBeInTheDocument();
-      const firstIngredientCheckbox = screen.getByRole('checkbox', { name: /lentils - 1 cup/i });
-      expect(firstIngredientCheckbox).not.toBeChecked();
-      userEvent.click(firstIngredientCheckbox);
-      expect(firstIngredientCheckbox).toBeChecked();
-      userEvent.click(firstIngredientCheckbox);
-      expect(screen.getByTestId('instructions')).toBeInTheDocument();
-      expect(screen.getByTestId(/finish-recipe-btn/)).toBeDisabled();
+      expect(screen.getByRole('heading', { name: /done recipes/i })).toBeInTheDocument();
+      expect(screen.getByRole('button', { name: /profile-icon/i })).toBeInTheDocument();
+      expect(screen.getByRole('button', { name: /all/i })).toBeInTheDocument();
+      expect(screen.getByRole('button', { name: /meals/i })).toBeInTheDocument();
+      expect(screen.getByRole('button', { name: /drinks/i })).toBeInTheDocument();
+      expect(screen.getByRole('img', { name: /corba/i })).toBeInTheDocument();
+      expect(screen.getByText(/turkish - side/i)).toBeInTheDocument();
+      expect(screen.getByText(/corba/i)).toBeInTheDocument();
+      const storage = JSON.parse(localStorage.getItem('doneRecipes'));
+      const { doneDate } = storage[0];
+      expect(screen.getByText(doneDate)).toBeInTheDocument();
+      expect(screen.getByText(/soup/i)).toBeInTheDocument();
+      expect(screen.getByRole('button', { name: /share/i })).toBeInTheDocument();
     });
 
-    // tests if favorite button works
+    // testing filters
+    userEvent.click(screen.getByRole('button', { name: /drinks/i }));
+
     await waitFor(() => {
-      const favoriteBtn = screen.getByRole('button', { name: /favorite icon/i });
-      const favoriteIcon = screen.getByRole('img', { name: /favorite icon/i });
-      expect(favoriteIcon).toHaveProperty('src', whiteHeartIcon);
-      userEvent.click(favoriteBtn);
-      expect(favoriteIcon).toHaveProperty('src', blackHeartIcon);
+      expect(screen.queryByRole('img', { name: /corba/i })).not.toBeInTheDocument();
     });
+
+    userEvent.click(screen.getByRole('button', { name: /meals/i }));
+
+    await waitFor(() => {
+      expect(screen.queryByRole('img', { name: /corba/i })).toBeInTheDocument();
+    });
+
+    userEvent.click(screen.getByRole('button', { name: /all/i }));
+
+    await waitFor(() => {
+      expect(screen.queryByRole('img', { name: /corba/i })).toBeInTheDocument();
+    });
+
+    // testing redirects
+
+    const mealRedirectButtons = screen.getAllByRole('button', { name: /corba/i });
+    expect(mealRedirectButtons).toHaveLength(2);
+
+    userEvent.click(mealRedirectButtons[0]);
+    expect(history.location.pathname).toBe('/meals/52977');
+
+    history.push('/done-recipes');
+
+    // expect(mealRedirectButtons[1]).toBeEnabled();
+    // userEvent.click(mealRedirectButtons[1]);
+
+    // await waitFor(() => {
+    //   expect(history.location.pathname).toBe('/meals/52977');
+    // });
   });
 
   it('ready to drink drinks should work properly', async () => {
-    const { history } = renderWithRouterAndContext(<App />, '/drinks');
 
-    await waitFor(() => {
-      const firstRecipeButton = screen.getByTestId(firstRecipeBtnID);
-      userEvent.click(firstRecipeButton);
-    });
-
-    expect(history.location.pathname).toBe(drinkPathname);
-
-    await waitFor(() => {
-      const startBtn = screen.getByRole('button', { name: /start recipe/i });
-      userEvent.click(startBtn);
-    });
-
-    expect(history.location.pathname).toBe('/drinks/15997/in-progress');
-
-    await waitFor(() => {
-      expect(screen.getByRole('heading', { name: /gg/i })).toBeInTheDocument();
-      expect(screen.getByRole('img', { name: /gg/i })).toHaveProperty('src', 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg');
-      expect(screen.getByRole('button', { name: /share icon/i })).toBeInTheDocument();
-      expect(screen.getByRole('button', { name: /favorite icon/i })).toBeInTheDocument();
-      expect(screen.getByRole('img', { name: /favorite icon/i })).toHaveProperty('src', whiteHeartIcon);
-      expect(screen.getByTestId('recipe-category')).toHaveTextContent('Optional alcohol');
-      expect(screen.getByText(/galliano - 2 1\/2 shots/i)).toBeInTheDocument();
-      const firstIngredientCheckbox = screen.getByRole('checkbox', { name: /galliano - 2 1\/2 shots/i });
-      expect(firstIngredientCheckbox).not.toBeChecked();
-      userEvent.click(firstIngredientCheckbox);
-      expect(firstIngredientCheckbox).toBeChecked();
-      userEvent.click(firstIngredientCheckbox);
-      expect(screen.getByTestId('instructions')).toBeInTheDocument();
-      expect(screen.getByTestId(/finish-recipe-btn/)).toBeDisabled();
-    });
-
-    // tests if favorite button works
-    await waitFor(() => {
-      const favoriteBtn = screen.getByRole('button', { name: /favorite icon/i });
-      screen.getByRole('button', { name: /share icon/i });
-      const favoriteIcon = screen.getByRole('img', { name: /favorite icon/i });
-      expect(favoriteIcon).toHaveProperty('src', whiteHeartIcon);
-      userEvent.click(favoriteBtn);
-      expect(favoriteIcon).toHaveProperty('src', blackHeartIcon);
-    });
   });
 });
