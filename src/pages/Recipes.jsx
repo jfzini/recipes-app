08b221bffb8e1545ef433b60aafe9d51046103fd
João Felipe Zini <joaofelipezini@gmail.com>
2023-06-26 10:54:34 -0300

    style: wip: recipes page css

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -4,39 +4,54 @@ import Header from '../components/Header';
 import RecipesContext from '../context/Context';
 import Footer from '../components/Footer';
 import './css/Recipes.css';
+import { act } from 'react-dom/test-utils';
 
 export default function Recipes() {
-  const {
-    recipes,
-    renderInitialRecipes,
-    filters,
-    renderFilters,
-    renderFilteredRecipes,
-  } = useContext(RecipesContext);
+  const { recipes, renderInitialRecipes, filters, renderFilters, renderFilteredRecipes } =
+    useContext(RecipesContext);
   const [activeFilter, setActiveFilter] = useState('');
+  const [loadingRecipes, setLoadingRecipes] = useState(true);
+  const [loadingFilters, setLoadingFilters] = useState(true);
   const history = useHistory();
   const { pathname } = history.location;
   const RECIPES_LIMIT = 11;
   const FILTERS_LIMIT = 4;
 
   useEffect(() => {
-    renderFilters(pathname);
-  }, [pathname, filters]);
-
-  useEffect(() => {
-    renderInitialRecipes(pathname);
+    async function fetchUpdatedFilters() {
+      setLoadingFilters(true);
+      await renderFilters(pathname);
+      setTimeout(() => {
+        setLoadingFilters(false);
+      }, 300);
+    }
+    fetchUpdatedFilters();
   }, [pathname]);
 
   useEffect(() => {
-    renderInitialRecipes(pathname);
-    renderFilters(pathname);
+    async function fetchUpdatedRecipes() {
+      setLoadingRecipes(true);
+      await renderInitialRecipes(pathname);
+      setLoadingRecipes(false);
+    }
+    fetchUpdatedRecipes();
+  }, [pathname]);
+
+  useEffect(() => {
+    async function fetchInitialData() {
+      setLoadingRecipes(true);
+      await renderInitialRecipes(pathname);
+      await renderFilters(pathname);
+      setLoadingRecipes(false);
+    }
+    fetchInitialData();
   }, []);
 
   const handleDetail = (id) => {
     history.push(`${pathname}/${id}`);
   };
 
-  const checkFilters = (filter=null) => {
+  const checkFilters = (filter = null) => {
     if (filter === activeFilter) {
       return 'filter-button active-filter';
     }
@@ -50,87 +65,103 @@ export default function Recipes() {
   };
 
   return (
-    <div className='recipe-page-container'>
+    <div className="recipe-page-container">
       <Header />
-      <div className='filters-container'>
-        <button
-          type="button"
-          data-testid="All-category-filter"
-          onClick={ () => {
-            renderInitialRecipes(pathname)
-            setActiveFilter('');
-          } }
-          className={checkFilters()}
-        >
-          All
-        </button>
-        {filters && filters.length > 1
-          ? filters.map((filter, index) => {
-            if (index <= FILTERS_LIMIT) {
-              return (
-                <button
-                  type="button"
-                  key={ index }
-                  className={checkFilters(filter.strCategory)}
-                  data-testid={ `${filter.strCategory}-category-filter` }
-                  onClick={ () => {
-                    if (activeFilter === filter.strCategory) {
-                      renderInitialRecipes(pathname);
-                      setActiveFilter('');
-                    } else {
-                      renderFilteredRecipes(pathname, filter.strCategory);
-                      setActiveFilter(filter.strCategory);
-                    }
-                  } }
-                >
-                  {filter.strCategory}
-                </button>
-              );
+      {loadingFilters ? (
+        <div>Loading filters...</div>
+      ) : (
+        <div className="filters-container">
+          <button
+            type="button"
+            data-testid="All-category-filter"
+            onClick={() => {
+              renderInitialRecipes(pathname);
+              setActiveFilter('');
+            }}
+            className={ activeFilter === ''
+              ? 'filter-button active-filter'
+              : 'filter-button inactive-filter'
             }
-            return '';
-          })
-          : ''}
-      </div>
-      <div>
+          >
+            All
+          </button>
+          {filters && filters.length > 1
+            ? filters.map((filter, index) => {
+                if (index <= FILTERS_LIMIT) {
+                  return (
+                    <button
+                      type="button"
+                      key={index}
+                      className={checkFilters(filter.strCategory)}
+                      data-testid={`${filter.strCategory}-category-filter`}
+                      onClick={async () => {
+                        if (activeFilter === filter.strCategory) {
+                          setActiveFilter('');
+                          setLoadingRecipes(true);
+                          await renderInitialRecipes(pathname);
+                          setLoadingRecipes(false);
+                        } else {
+                          setActiveFilter(filter.strCategory);
+                          setLoadingRecipes(true);
+                          await renderFilteredRecipes(pathname, filter.strCategory);
+                          setLoadingRecipes(false);
+                        }
+                      }}
+                    >
+                      {filter.strCategory}
+                    </button>
+                  );
+                }
+                return '';
+              })
+            : ''}
+        </div>
+      )}
+      {loadingRecipes ? <div>Loading recipes...</div> : 
+      <div className='recipes-container'>
         {recipes && recipes.length >= 1
           ? recipes.map((recipe, index) => {
-            if (index <= RECIPES_LIMIT) {
-              return (
-                <div
-                  data-testid={ `${index}-recipe-card` }
-                  key={ recipe.idDrink || recipe.idMeal }
-                >
-                  <button
-                    type="button"
-                    data-testid={ `${index}-recipe-button` }
-                    onClick={ () => {
+              if (index <= RECIPES_LIMIT) {
+                return (
+                  <div
+                    data-testid={`${index}-recipe-card`}
+                    key={recipe.idDrink || recipe.idMeal}
+                    className="recipe-card"
+                    onClick={() => {
                       if (recipe.idDrink) {
                         handleDetail(recipe.idDrink);
                       }
                       if (recipe.idMeal) {
                         handleDetail(recipe.idMeal);
                       }
-                    } }
+                    }}
                   >
-                    <img
-                      src={ recipe.strDrinkThumb || recipe.strMealThumb }
-                      alt=""
-                      data-testid={ `${index}-card-img` }
-                    />
-                  </button>
-                  <p
-                    data-testid={ `${index}-card-name` }
-                  >
-                    {recipe.strDrink || recipe.strMeal}
-                  </p>
-                </div>
-              );
-            }
-            return '';
-          })
+                    <div
+                      data-testid={`${index}-recipe-button`}
+                      className='recipe-image-container'
+                    >
+                      <img
+                        src={recipe.strDrinkThumb || recipe.strMealThumb}
+                        alt={recipe.strDrink || recipe.strMeal}
+                        data-testid={`${index}-card-img`}
+                        className='recipe-image'
+                      />
+                    </div>
+                    <p
+                      data-testid={`${index}-card-name`}
+                      className='recipe-name'
+                    >
+                      {(recipe.strDrink || recipe.strMeal).slice(0, 24)}
+                    </p>
+                  </div>
+                );
+              }
+              return '';
+            })
           : ''}
       </div>
-      <Footer />
+      }
+      {loadingFilters && loadingRecipes ? '' : <Footer />}
     </div>
   );
 }
