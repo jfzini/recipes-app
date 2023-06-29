8aeff43b0f05a43372e1e85d19436afbacd3beff
João Felipe Zini <joaofelipezini@gmail.com>
2023-06-29 08:36:32 -0300

    style: buttons

--- src/components/RecipeData.jsx
+++ src/components/RecipeData.jsx
@@ -28,9 +28,7 @@ export default function RecipeData({
   const CAROUSEL_LIMIT = 5;
 
   const handleCopy = (path, recipeID) => {
-    const filteredPathname = path.includes('in-progress')
-      ? path.replace('/in-progress', '')
-      : path;
+    const filteredPathname = path.includes('in-progress') ? path.replace('/in-progress', '') : path;
     copy(`http://localhost:3000${filteredPathname}`);
     setLinkCopied(recipeID);
     setTimeout(() => {
@@ -39,142 +37,145 @@ export default function RecipeData({
   };
 
   return (
-    <div className='detail-page-container'>
-      {recipe && recipe.map((recipeData) => (
-        <div key={ id }>
-          <div className='detail-hero'>
-            <h2 data-testid="recipe-title" className='detail-title'>
-              {recipeData.strMeal || recipeData.strDrink}
-            </h2>
-            <img
-              src={ recipeData.strMealThumb || recipeData.strDrinkThumb }
-              alt={ recipeData.strMeal || recipeData.strDrink }
-              data-testid="recipe-photo"
-              className='detail-img'
-            />
+    <div className="detail-page-container">
+      {recipe &&
+        recipe.map((recipeData) => (
+          <div key={id}>
+            <div className="detail-hero">
+              <h2 data-testid="recipe-title" className="detail-title">
+                {recipeData.strMeal || recipeData.strDrink}
+              </h2>
+              <img
+                src={recipeData.strMealThumb || recipeData.strDrinkThumb}
+                alt={recipeData.strMeal || recipeData.strDrink}
+                data-testid="recipe-photo"
+                className="detail-img"
+              />
+              <p data-testid="recipe-category" className="recipe-category">
+                {pathname.includes('/meals') ? recipeData.strCategory : recipeData.strAlcoholic}
+              </p>
+            </div>
             <button
               data-testid="share-btn"
-              onClick={ () => handleCopy(pathname, id) }
-              className='share-btn'
+              onClick={() => handleCopy(pathname, id)}
+              className="share-btn"
             >
               <img
-                src={ linkCopied === id ? filledCopyIcon : emptyCopyIcon }
+                src={linkCopied === id ? filledCopyIcon : emptyCopyIcon}
                 alt="share icon"
-                className='detail-icons'
+                className="detail-icons"
               />
             </button>
-            <button onClick={ handleFavorites } className='favorite-btn'>
+            <button onClick={handleFavorites} className="favorite-btn">
               {favorite ? (
                 <img
-                  src={ filledFavoriteIcon }
+                  src={filledFavoriteIcon}
                   alt="favorite icon"
                   data-testid="favorite-btn"
-                  className='detail-icons'
+                  className="detail-icons"
                 />
               ) : (
                 <img
-                  src={ emptyFavoriteIcon }
+                  src={emptyFavoriteIcon}
                   alt="favorite icon"
                   data-testid="favorite-btn"
-                  className='detail-icons'
+                  className="detail-icons"
                 />
               )}
             </button>
-            <p data-testid="recipe-category" className='recipe-category'>
-              {pathname.includes('/meals')
-                ? recipeData.strCategory
-                : recipeData.strAlcoholic}
-            </p>
-          </div>
-          <ul className='ingredients-list'>
-            <h3 className='subtitle'>Ingredients</h3>
-            {Object.keys(recipeData)
-              .filter((key) => key.includes('strIngredient') && recipeData[key])
-              .map((ingredient, index) => {
-                if (type === 'progress') {
+            <ul className="ingredients-list">
+              <h3 className="subtitle">Ingredients</h3>
+              {Object.keys(recipeData)
+                .filter((key) => key.includes('strIngredient') && recipeData[key])
+                .map((ingredient, index) => {
+                  if (type === 'progress') {
+                    return (
+                      <div key={index}>
+                        <label
+                          htmlFor={`${index}-ingredient-checkbox`}
+                          data-testid={`${index}-ingredient-step`}
+                          className={
+                            usedIngredients.includes(recipeData[ingredient])
+                              ? 'checked-ingredient'
+                              : ''
+                          }
+                        >
+                          {recipeData[ingredient]}
+                          {' - '}
+                          {recipeData[ingredient.replace('strIngredient', 'strMeasure')]}
+                          <input
+                            type="checkbox"
+                            id={`${index}-ingredient-checkbox`}
+                            onChange={() => handleUsedIngredients(recipeData[ingredient])}
+                            checked={usedIngredients.includes(recipeData[ingredient])}
+                          />
+                        </label>
+                      </div>
+                    );
+                  }
                   return (
-                    <div key={ index }>
-                      <label
-                        htmlFor={ `${index}-ingredient-checkbox` }
-                        data-testid={ `${index}-ingredient-step` }
-                        className={
-                          usedIngredients.includes(recipeData[ingredient])
-                            ? 'checked-ingredient'
-                            : ''
-                        }
-                      >
-                        {recipeData[ingredient]}
-                        {' - '}
-                        {recipeData[ingredient.replace('strIngredient', 'strMeasure')]}
-                        <input
-                          type="checkbox"
-                          id={ `${index}-ingredient-checkbox` }
-                          onChange={ () => handleUsedIngredients(recipeData[ingredient]) }
-                          checked={ usedIngredients.includes(recipeData[ingredient]) }
-                        />
-                      </label>
-                    </div>
+                    <li data-testid={`${index}-ingredient-name-and-measure`} key={`${index}-li`}>
+                      {recipeData[ingredient]}
+                      {' - '}
+                      {recipeData[ingredient.replace('strIngredient', 'strMeasure')]}
+                    </li>
                   );
-                }
-                return (
-                  <li
-                    data-testid={ `${index}-ingredient-name-and-measure` }
-                    key={ `${index}-li` }
-                  >
-                    {recipeData[ingredient]}
-                    {' - '}
-                    {recipeData[ingredient.replace('strIngredient', 'strMeasure')]}
-                  </li>
-                );
-              })}
-          </ul>
-          <div className='instructions'>
-            <h3 className='subtitle'>Instructions</h3>
-            <p data-testid="instructions" className='instructions-body'>{recipeData.strInstructions}</p>
-          </div>
-          {pathname.includes('meals') && (
-            <div className='video-container'>
-              <h3 className='subtitle'>Tutorial</h3>
-              <iframe
-                title={ recipeData.strMeal }
-                src={ recipeData.strYoutube && recipeData.strYoutube.replace('watch?v=', 'embed/') }
-                data-testid="video"
-                className='video'
-              />
+                })}
+            </ul>
+            <div className="instructions">
+              <h3 className="subtitle">Instructions</h3>
+              <p data-testid="instructions" className="instructions-body">
+                {recipeData.strInstructions}
+              </p>
             </div>
-          )}
-          {type === 'progress' && (
+            {pathname.includes('meals') && (
+              <div className="video-container">
+                <h3 className="subtitle">Tutorial</h3>
+                <iframe
+                  title={recipeData.strMeal}
+                  src={recipeData.strYoutube && recipeData.strYoutube.replace('watch?v=', 'embed/')}
+                  data-testid="video"
+                  className="video"
+                />
+              </div>
+            )}
+            {type === 'progress' && (
+              <div className='finish-recipe-btn-container'>
+                <button
+                  data-testid="finish-recipe-btn"
+                  disabled={
+                    Object.keys(recipeData).filter(
+                      (key) => key.includes('strIngredient') && recipeData[key],
+                    ).length !== usedIngredients.length
+                  }
+                  onClick={() => finishRecipe(recipeData)}
+                  className="finish-recipe-btn"
+                >
+                  Finalizar receita
+                </button>
+              </div>
+            )}
+          </div>
+        ))}
+        {type === 'details' ? (
+          <>
+            <h3 className="suggestion-subtitle">
+              Suggested {pathname.includes('meals') ? 'drinks' : 'meals'}
+            </h3>
+            <Carousel suggestions={suggestions} CAROUSEL_LIMIT={CAROUSEL_LIMIT} />
             <button
-              data-testid="finish-recipe-btn"
-              disabled={
-                Object.keys(recipeData).filter(
-                  (key) => key.includes('strIngredient') && recipeData[key],
-                ).length !== usedIngredients.length
-              }
-              onClick={ () => finishRecipe(recipeData) }
+              type="button"
+              data-testid="start-recipe-btn"
+              className="start-recipe-btn"
+              onClick={() => {
+                startRecipe();
+                history.push(`${pathname}/in-progress`);
+              }}
             >
-              Finalizar receita
+              {inProgress ? 'Continue Recipe' : 'Start Recipe'}
             </button>
-          )}
-        </div>
-      ))}
-      <div>
-      <h3 className='suggestion-subtitle'>Suggested {pathname.includes('meals') ? 'drinks' : 'meals'}</h3>
-      <Carousel suggestions={suggestions} CAROUSEL_LIMIT={CAROUSEL_LIMIT}/>
-      </div>
-      {type === 'details'
-      && (
-        <button
-          type="button"
-          data-testid="start-recipe-btn"
-          className="start-recipe-btn"
-          onClick={ () => {
-            startRecipe();
-            history.push(`${pathname}/in-progress`);
-          } }
-        >
-          { inProgress ? 'Continue Recipe' : 'Start Recipe'}
-        </button>)}
+          </>
+        ) : ''}
     </div>
   );
 }
